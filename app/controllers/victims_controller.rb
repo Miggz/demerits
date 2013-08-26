c3fdeedff0b894ddeb4f3be25f7ed0eb8190de2d
Miggz <mmyers@g2crowd.com>
2013-08-25 19:36:03 -0500

    responsive victim show with media qureees

--- app/controllers/victims_controller.rb
+++ app/controllers/victims_controller.rb
@@ -70,6 +70,6 @@ class VictimsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def victim_params
-      params.require(:victim).permit(:name, :email)
+      params.require(:victim).permit(:name, :image)
     end
 end
